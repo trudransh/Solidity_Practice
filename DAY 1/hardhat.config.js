5495383f0413a298c7abeb00ff5fccd448b6bba2
Rudransh Singh Tomar <trudranshsingh2003@gmail.com>
2023-10-10 17:28:44 +0530

    Updated configurations and package.json

--- DAY 1/hardhat.config.js
+++ DAY 1/hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.11",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
